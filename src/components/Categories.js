31998b73539b554a4e000b08001c4a01b763ad7f
Aditya Nama <92026058+adityanama@users.noreply.github.com>
2024-05-31 16:19:28 +0530

    ok

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -7,7 +7,7 @@ const Categories = () => {
 
         <div className='flex w-100 -mt-32'>
             <img src='https://www.freepnglogos.com/uploads/windows-logo-png/windows-logo-logok-0.png' className='scale-50 -ml-32'></img>
-            <img src='https://static-00.iconduck.com/assets.00/playstation-icon-512x417-c27cqp2p.png' className=' scale-30 -ml-[200px] scale-x-50'></img>
+            <img src='https://static-00.iconduck.com/assets.00/playstation-icon-512x417-c27cqp2p.png' className=' scale-30 -ml-[230px] scale-x-50'></img>
             <img src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/387_Xbox_logo-512.png' className='scale-30 -ml-16 scale-x-40'></img>
             
         </div>
