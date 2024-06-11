b301fd3cfcaf68c28972d814d484862066d38af0
Abhinav Arora <abhinavarora310@gmail.com>
2024-06-11 17:34:55 +0530

    init

--- src/components/NewRelease.js
+++ src/components/NewRelease.js
@@ -4,8 +4,8 @@ import {GameData} from '../GamesData'
 
 const NewRelease = () => {
     return (
-        <div className='flex flex-col justify-center items-center gap-24' id='new-release'>
-            <h1 className='text-[55px] text-white text-center heading -mt-2'>New Releases</h1>
+        <div className='flex flex-col justify-center items-center gap-24' >
+            <h1 className='text-[55px] text-white text-center heading -mt-2' id='new-release'>New Releases</h1>
             <div className='flex w-11/12 justify-between mx-auto mb-32 flex-wrap items-center'>
                 {
                     GameData[2].NewRelease.map((GameData) => (
